69b0e1bc3ab4355758f3fe8922fcf4d904f6ba43
Sophie Gilder <sophieachannon@hotmail.co.uk>
2023-04-16 18:34:47 +0100

    returns command type

--- projects/07/VirtualMachine/src/Parser.ts
+++ projects/07/VirtualMachine/src/Parser.ts
@@ -1,15 +1,45 @@
-import * as fs from "fs"
-
+import * as fs from "fs";
 
 export default class Parser {
   file: string[];
+  counter: number;
+  currentCommand?: string;
 
   constructor(filePath: string) {
-    const openFile = fs.readFileSync(filePath, 'utf8');
-    this.file = openFile.split('\n');
+    const openFile = fs.readFileSync(filePath, "utf8");
+    this.file = openFile.split("\n");
+    this.counter = 0;
+    this.currentCommand = undefined;
   }
 
-  getFile() {    
-    return this.file
+  getFile() {
+    return this.file;
+  }
+
+  hasMoreCommands() {
+    return this.counter < this.file.length;
+  }
+
+  advance() {
+    this.currentCommand = this.file[this.counter];
+    this.counter++;
+  }
+
+  commandType() {
+    const command = this.currentCommand?.split(" ")[0];
+    const arithmetic = [
+      "add",
+      "sub",
+      "neg",
+      "eq",
+      "gt",
+      "lt",
+      "and",
+      "or",
+      "not",
+    ];
+    if (command && arithmetic.includes(command)) return "C_ARITHMETIC";
+    if (command === "if-goto") return "C_IF";
+    return `C_${command?.toUpperCase()}`;
   }
 }
--- projects/07/VirtualMachine/src/__tests__/Parser.test.ts
+++ projects/07/VirtualMachine/src/__tests__/Parser.test.ts
@@ -5,4 +5,17 @@ describe("Parser", () => {
     const parser = new Parser("src/__tests__/Sample.vm");
     expect(parser.getFile()).toEqual(["first line", "second line"]);
   });
+  it("advances and returns commandType", () => {
+    const parser = new Parser("src/__tests__/CommandTypes.vm");
+    parser.advance();
+    expect(parser.commandType()).toEqual("C_ARITHMETIC");
+    parser.advance();
+    expect(parser.commandType()).toEqual("C_ARITHMETIC");
+    parser.advance();
+    expect(parser.commandType()).toEqual("C_PUSH");
+    parser.advance();
+    expect(parser.commandType()).toEqual("C_IF");
+    parser.advance();
+    expect(parser.commandType()).toEqual("C_RETURN");
+  });
 });
